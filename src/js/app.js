7d7267624a6bf38d6f92a5dcae2e5f18a42140c4
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2020-12-11 18:23:37 +0100

    Divide JS into components, update the files and add abilities for subpages

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,52 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
-  init: function(){
+  initPages: function(){
     const thisApp = this;
 
-    thisApp.initData();
-    thisApp.initCart();
-  },
-  initData: function(){
-    const thisApp = this;
-    const url = settings.db.url + '/' + settings.db.product;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
-    thisApp.data = {};
+    const idFromHash = window.location.hash.replace('#/', '');
 
-    fetch(url)
-      .then(function(rawResponse){
-        return rawResponse.json();
-      })
-      .then(function(parsedResponse){
-        thisApp.data.products = parsedResponse;
-        thisApp.initMenu();
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
       });
+    }
+  },
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
   },
   initMenu: function(){
     const thisApp = this;
@@ -42,6 +66,28 @@ const app = {
     thisApp.productList.addEventListener('add-to-cart', function(event){
       app.cart.add(event.detail.product);
     });
+  },
+  initData: function(){
+    const thisApp = this;
+    const url = settings.db.url + '/' + settings.db.product;
+
+    thisApp.data = {};
+
+    fetch(url)
+      .then(function(rawResponse){
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse){
+        thisApp.data.products = parsedResponse;
+        thisApp.initMenu();
+      });
+  },
+  init: function(){
+    const thisApp = this;
+
+    thisApp.initPages();
+    thisApp.initData();
+    thisApp.initCart();
   },
 };
 
